4aa69fb6436ada857f9182190ea6d6e06adb2be9
Eduardo N Gomes <Eduardo.000@live.com>
2022-10-19 15:22:03 -0300

    Create UserController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,23 +1,25 @@
 const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
 
 class UsersController {
   async create(request, response) {
     const { name, email, password, admin } = request.body
 
     if (!name || !email || password.length < 6) {
-      throw new Error('Informacoes invalidas')
+      throw new AppError('Informações inválidas')
     }
 
-    try {
-      await knex('users').insert({
-        name,
-        email,
-        password,
-        admin
-      })
-    } catch (error) {
-      response.status(400).send(error)
+    const userExists = await knex.select('email').where({ email }).from('users')
+
+    if (userExists.length === 1) {
+      throw new AppError('Este email já está em uso', 401)
     }
+    await knex('users').insert({
+      name,
+      email,
+      password,
+      admin
+    })
 
     return response.status(200).send('User created')
   }
