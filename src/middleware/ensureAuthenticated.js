39730f0f05cee0517f0e9af1e9997d3f7be75f4d
Eduardo N Gomes <Eduardo.000@live.com>
2022-10-24 15:08:02 -0300

    Create middleware authenticated

--- src/middleware/ensureAuthenticated.js
+++ src/middleware/ensureAuthenticated.js
@@ -3,4 +3,25 @@ const AppError = require('../utils/AppError')
 
 const authConfig = require('../configs/auth')
 
-function ensureAuthenticated(request, response, next) {}
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    throw new AppError('JWT token inválido')
+  }
+
+  const [, token] = authHeader.split(' ')
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret)
+
+    request.user = {
+      id: Number(user_id)
+    }
+
+    return next()
+  } catch {
+    throw new AppError('JWT token inválido')
+  }
+}
+
+module.exports = ensureAuthenticated
