7ff544cf737bd79c9620af8f0eb3678302858db4
Eduardo N Gomes <Eduardo.000@live.com>
2022-10-24 15:00:28 -0300

    Create SessionsController

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -7,3 +7,32 @@ const { compare } = require('bcryptjs')
 const authConfig = require('../configs/auth')
 
 const { sign } = require('jsonwebtoken')
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body
+
+    const user = await knex('users').where({ email }).first()
+
+    if (!user) {
+      throw new AppError('email e/ou senha incorreta')
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new AppError('email e/ou senha incorreta')
+    }
+
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
+  }
+}
+
+module.exports = SessionsController
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const platesRoutes = require('./plates.routes')
 const usersRoutes = require('./users.routes')
 const ordersRoutes = require('./orders.routes')
 const favoritesRoutes = require('./favorites.routes')
+const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 
@@ -11,5 +12,6 @@ routes.use('/plates', platesRoutes)
 routes.use('/users', usersRoutes)
 routes.use('/orders', ordersRoutes)
 routes.use('/favorites', favoritesRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 module.exports = routes
